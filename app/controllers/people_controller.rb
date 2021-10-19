fda329377a4f3ada9af28099b5dccbe205c67fa1
Sarah Thiery <sarahvthiery@gmail.com>
2021-10-19 13:19:35 -0700

    Update room, bed, and person controllers

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,27 @@
 class PeopleController < ApplicationController
+    def index
+        @people = Person.all
+        render json: @people
+    end
+
+    def show
+        @person = Person.find(params[:id])
+        render json: @person
+    end
+
+    def create
+        @person = Person.create(person_params)
+        render json: @person
+    end
+
+    def update
+        @person.update(person_params)
+        render json: @person
+    end
+
+    private
+
+    def person_params
+        params.permit(:full_name, :email, :bed_id)
+    end
 end
