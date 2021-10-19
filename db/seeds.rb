b04602588ce182f8769c31db26c0152dec6c5d1a
Sarah Thiery <sarahvthiery@gmail.com>
2021-10-19 10:53:15 -0700

    Add to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ Room.destroy_all
 Bed.destroy_all
 Person.destroy_all 
 
-one = Building.create(name: "La Suite de l'Orangerie", location: "le parc", price: , category: , capacity: , format: , description: , available?: true)
+one = Building.create(name: "La Suite de l'Orangerie", location: "Le parc", price: , category: , capacity: , format: , description: , available?: true)
 
-two = Building.create(name: "Au Pigeonnier", location: "le parc", price: 350, category: "maison", capacity: "7 people", format: "
+two = Building.create(name: "Au Pigeonnier", location: "Le parc", price: 350, category: "House", capacity: "7 people", format: "
 living room / bedroom, 2 bedrooms,
 1 bathroom with bath/shower, 1 bathroom with shower, 1 terrace in the courtyard", description: "
 
@@ -33,19 +33,93 @@ Wifi is available.
 The Kitchen is equipped as follows:
 A fridge freezer, dishwasher, microwave oven, induction hob, oven,
 toaster, kettle, coffee machine, and crockery adapted to the capacity of the house.
-Capsules of coffee, tea, water are at your disposal for an early breakfast, or other at your
-convenience.
+Capsules of coffee, tea, water are at your disposal.
 The bathroom & shower room are equipped as follows:
 Towels are provided.
 1 hair dryer and 1 mirror in the bathroom.
 1 hair dryer and 1 mirror in the bathroom", available?: true)
 
-three = Building.create(name: , location: , price: , category: , capacity: , format: , description: , available?: true)
-four = Building.create(name: , location: , price: , category: , capacity: , format: , description: , available?: true)
-five = Building.create(name: , location: , price: , category: , capacity: , format: , description: , available?: true)
+three = Building.create(name: "L'Ecurie", location: "Le parc", price: 900, category: "Apartments", capacity: "18 people", format: "
+1 living room / kitchen / bedroom, 
+6 bedrooms,
+1 terrace,
+ 3 shower rooms / wc", description: "
+ The accommodation is located on the 1st floor: 
+ 3 double rooms, 3 twin rooms, 2 triple rooms,
+ 3 shower rooms with wc
+ 
+To make your stay pleasant, beds are made on your arrival.
+
+For your preparations: An ironing board and iron are at your disposal.
+
+The Kitchen is equipped as follows:
+A fridge freezer, dishwasher, microwave oven, induction hob, oven,
+kettle, coffee machine, and crockery adapted to the capacity of the lodging.
+Capsules of coffee, tea, water are at your disposal.
+
+The bathrooms are equipped as follows:
+Towels are provided.
+1 hair dryer and 1 mirror in each bathroom", available?: true)
+
+four = Building.create(name: "La Maison des Gardes", location: "Le parc", price: 400, category: "Independent house with terrace", capacity: "7 people", format: "
+living room / bedrooms, 
+3 bedrooms, 
+1 bathroom
+with wc, 
+1 bathroom, 
+1 separate / independent wc", description: "
+- on the ground floor
+1 bedroom with 2 single beds,
+1 living room (2 additional beds possible in the sofa bed)
+A fully-equipped kitchen
+1 shower room, separate toilet.
+- on the 1st floor
+1 bedroom with 2 bunk beds and 1 single bed,
+1 bedroom with 2 single beds,
+1 bathroom with wc
+
+To make your stay pleasant, beds are made on your arrival.
+For your preparations: An ironing board and iron are at your disposal. A full mirror is installed.
+The Kitchen is equipped as follows:
+A fridge freezer, dishwasher, microwave oven, induction hob, oven,
+toaster, kettle, coffee machine, and crockery adapted to the capacity of the house.
+Capsules of coffee, tea, water are at your disposal.
+The bathroom & shower room are equipped as follows:
+Towels are provided.
+1 hair dryer and 1 mirror in the bathroom.
+1 hair dryer and 1 mirror in the bathroom", available?: true)
+
+five = Building.create(name: "L’Epi de Blé", location: "La ferme d'à côté", price: 450, category: "Independent house", capacity: "9 people", format: "
+living room / bedrooms, 3 bedrooms,
+1 bathroom with wc, 1 shower room,
+1 separate / independent wc
+1 garden & a courtyard, 1 garage", description: "- on the ground floor
+1 bedroom with 2 single beds,
+1 living room: 1 convertible sofa bed for 2 people, the sleeping area is separated from the dining room through a sliding screen, it opens onto an equipped kitchen,
+1 shower room, separate toilet.
+- on the 1st floor
+1 bedroom with 3 single beds,
+1 bedroom with 2 single beds,
+1 bathroom with wc.
+
+To make your stay pleasant, beds are made on your arrival.
+For your preparations: An ironing board and iron are at your disposal. A full mirror is installed.
+The Kitchen is equipped as follows:
+A fridge freezer, dishwasher, microwave oven, induction hob, oven,
+toaster, kettle, coffee machine, and crockery adapted to the capacity of the house.
+Capsules of coffee, tea, water are at your disposal.
+The bathroom & shower room are equipped as follows:
+1 hair dryer and 1 mirror in the bathroom.
+1 hair dryer and 1 mirror in the bathroom
+Towels are provided.", available?: true)
+
 six = Building.create(name: , location: , price: , category: , capacity: , format: , description: , available?: true)
+
 seven = Building.create(name: , location: , price: , category: , capacity: , format: , description: , available?: true)
+
 eight = Building.create(name: , location: , price: , category: , capacity: , format: , description: , available?: true)
+
 nine = Building.create(name: , location: , price: , category: , capacity: , format: , description: , available?: true)
+
 ten = Building.create(name: , location: , price: , category: , capacity: , format: , description: , available?: true)
 
